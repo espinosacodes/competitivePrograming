fa611fe26b1519ea04fd9bd55a294e1d83fbca8f
espinosacodes <santiagoespinosagiraldo1@gmail.com>
2025-04-04 11:43:41 -0500

    refactor H. Blurry Vision.cpp to pass the other tests

--- cppCodeforces/H. Blurry Vision.cpp
+++ cppCodeforces/H. Blurry Vision.cpp
@@ -10,23 +10,21 @@ int main() {
         "SYSTEM",
         "APRIL",
         "FOOLS",
-        "THE",
-        "ONLY",
-        "THING",
-        "IS",
-        "CONSTANT"
+        "YOU",
+        "READ",
+        "POORLY",
+        "GET",
+        "EYEGLASSES"
     };
 
     int x;
     cin >> x;
 
-    // Validate input
     if (x < 1 || x > 11) {
         cout << "Invalid input" << endl;
-        return 1; // Exit with error code
+        return 1;
     }
 
-    // Output the word on the x-th line
     cout << lines[x - 1] << endl;
 
     return 0;
